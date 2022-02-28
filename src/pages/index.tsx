a2af506b9bde1fa1b7eef5590f7259107483a4e7
Odilon Ferreira <odilon.aferreira@gmail.com>
2022-02-28 19:49:49 -0300

    installing sass and working with title with next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import styles from "../styles/home.module.scss";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
-  )
+    <>
+      <Head>
+        <title>Início | ignews</title>
+      </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
+  );
 }
