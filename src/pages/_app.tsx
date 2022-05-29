0cdbff429d5b1bc5a6947dcac3dee01c75310a07
Odilon Ferreira <odilon.aferreira@gmail.com>
2022-05-29 20:47:43 -0300

    requesting posts from prismic api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,29 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
+import { PrismicProvider } from "@prismicio/react";
+import Link from "next/link";
 
 import "../styles/global.scss";
+import { linkResolver, repositoryName } from "../services/prismic";
+import { PrismicPreview } from "@prismicio/next";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+          <Link href={href}>
+            <a {...props}>{children}</a>
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Header />
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>
     </SessionProvider>
   );
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { createClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  id: string;
+  slug: string;
+  title: string;
+  summary: string;
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,27 +25,41 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>26 de abril de 2022</time>
-            <strong>Título do post em si</strong>
-            <p>
-              Conteúdo do post Conteúdo do post Conteúdo do post Conteúdo do
-              Conteúdo do post Conteúdo do post Conteúdo do post post Conteúdo
-              do post{" "}
-            </p>
-          </a>
-          <a>
-            <time>26 de abril de 2022</time>
-            <strong>Título do post em si</strong>
-            <p>Conteúdo do post</p>
-          </a>
-          <a>
-            <time>26 de abril de 2022</time>
-            <strong>Título do post em si</strong>
-            <p>Conteúdo do post</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.id}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.summary}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const prismic = createClient({ previewData });
+  const response = await prismic.getAllByType("post", {
+    fetch: ["post.title", "post.content"],
+    pageSize: 100,
+  });
+  
+  const posts = response && response.map((post) => {
+    return {
+      id: post.id,
+      slug: post.uid,
+      title: RichText.asText(post?.data?.title),
+      summary: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
+  return {
+    props: {posts},
+  };
+};
